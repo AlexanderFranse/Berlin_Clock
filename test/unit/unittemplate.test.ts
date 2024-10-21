b683d79b1d13d0d0f096ce960907e5933921860d
alexander.franse <alexander.franse@ns.nl>
2024-10-21 10:35:35 +0200

    build: added first validation test that checks if strings are not empty

--- test/unit/unittemplate.test.ts
+++ test/unit/unittemplate.test.ts
@@ -1,7 +1,9 @@
-import { templateFunction } from "../../src/template";
+import { isStringNotEmpty } from "../../src/functions";
 
-describe("TemplateFunction should return half of the provided", () => {
-  it("2 -> 1 ", () => {
-    expect(templateFunction(2)).toEqual(1);
+describe("There is need to determine the order of the braces. Braces have different shapes like (), [] or {}. Every brace that opens should have a closing brace as well", () => {
+  describe("First thing to do is to validate if there is actually a brace existing", () => {
+    it("(){}[] --> true", () => {
+      expect(isStringNotEmpty("(){}[]")).toBe(true);
+    });
   });
 });
